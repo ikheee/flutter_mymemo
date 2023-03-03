9666f31a1044f266b73817836cf6dfeb1bcde278
shinikhee <ikheee@naver.com>
2023-03-04 02:00:48 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'memo_service.dart';
 
-void main() {
+late SharedPreferences prefs;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  prefs = await SharedPreferences.getInstance();
   // runApp(const MyApp());
   runApp(
     MultiProvider(
@@ -50,52 +55,66 @@ class _HomePageState extends State<HomePage> {
         body: memoList.isEmpty
             ? Center(child: Text('메모를 작성해 주세요.'))
             : ListView.builder(
-                itemCount: memoList.length, // memoList 개수 만큼 보여주기
+                itemCount: memoList.length,
                 itemBuilder: (context, index) {
-                  Memo memo = memoList[index]; // index에 해당하는 memo 가져오기
-                  return ListTile(
-                    // 메모 고정 아이콘
-                    leading: IconButton(
-                      icon: Icon(CupertinoIcons.pin),
-                      onPressed: () {
-                        print('$memo : pin 클릭 됨');
-                      },
-                    ),
-                    // 메모 내용 (최대 3줄까지만 보여주도록)
-                    title: Text(
-                      memo.content,
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    subtitle: Text('subtitle'),
-                    trailing: Icon(Icons.access_alarms_outlined),
-                    onTap: () {
-                      // 아이템 클릭시
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) => DetailPage(
-                            index: index,
-                          ),
+                  Memo memo = memoList[index];
+                  return Column(
+                    children: [
+                      ListTile(
+                        leading: IconButton(
+                          icon: Icon(memo.isPinned
+                              ? CupertinoIcons.pin_fill
+                              : CupertinoIcons.pin),
+                          onPressed: () {
+                            memoService.updatePinMemo(index: index);
+                          },
                         ),
-                      );
-                    },
+                        title: Text(
+                          memo.content,
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        subtitle: Text(
+                          memo.updatedAt == null
+                              ? ''
+                              : memo.updatedAt.toString().substring(0, 19),
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        trailing: Icon(Icons.access_alarms_outlined),
+                        onTap: () async {
+                          await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => DetailPage(
+                                index: index,
+                              ),
+                            ),
+                          );
+                          if (memo.content.isEmpty) {
+                            memoService.deleteMemo(index: index);
+                          }
+                        },
+                      ),
+                      Divider(),
+                    ],
                   );
                 },
               ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: () {
-            // + 버튼 클릭시 메모 생성 및 수정 페이지로 이동
+          onPressed: () async {
             memoService.createMemo(content: '');
-            Navigator.push(
+            await Navigator.push(
               context,
               MaterialPageRoute(
                 builder: (_) => DetailPage(
-                  index: memoService.memoList.length - 1,
+                  index: memoList.length - 1,
                 ),
               ),
             );
+            if (memoList.last.content.isEmpty) {
+              memoService.deleteMemo(index: memoList.length - 1);
+            }
           },
         ),
       );
@@ -125,38 +144,7 @@ class DetailPage extends StatelessWidget {
           IconButton(
             onPressed: () {
               // 삭제 버튼 클릭시
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    title: Text("정말로 삭제하시겠습니까?",
-                        style: TextStyle(
-                          fontSize: 14,
-                        )),
-                    actions: [
-                      // 취소 버튼
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: Text("취소"),
-                      ),
-                      // 확인 버튼
-                      TextButton(
-                        onPressed: () {
-                          memoService.deleteMemo(index: index);
-                          Navigator.pop(context); // 팝업 닫기
-                          Navigator.pop(context); // HomePage 로 가기
-                        },
-                        child: Text(
-                          "확인",
-                          style: TextStyle(color: Colors.pink),
-                        ),
-                      ),
-                    ],                    
-                  );
-                },
-              );
+              showDeleteDialog(context, memoService);
             },
             icon: Icon(Icons.delete),
           )
@@ -176,10 +164,45 @@ class DetailPage extends StatelessWidget {
           keyboardType: TextInputType.multiline,
           onChanged: (value) {
             // 텍스트필드 안의 값이 변할 때
-            memoService.updateMemo(index: index, content: value);            
+            memoService.updateMemo(index: index, content: value);
           },
         ),
       ),
+    );
+  }
+
+  void showDeleteDialog(BuildContext context, MemoService memoService) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("정말로 삭제하시겠습니까?",
+              style: TextStyle(
+                fontSize: 14,
+              )),
+          actions: [
+            // 취소 버튼
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("취소"),
+            ),
+            // 확인 버튼
+            TextButton(
+              onPressed: () {
+                memoService.deleteMemo(index: index);
+                Navigator.pop(context); // 팝업 닫기
+                Navigator.pop(context); // HomePage 로 가기
+              },
+              child: Text(
+                "확인",
+                style: TextStyle(color: Colors.pink),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
